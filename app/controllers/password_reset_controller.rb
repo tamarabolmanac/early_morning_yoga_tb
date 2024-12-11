8b692e09b8297773396b969643d6b31d27333621
Tamara Burcak <tamaraburcak@Tamaras-MacBook-Air.local>
2024-12-11 22:38:44 +0100

    Corrrect

--- app/controllers/password_reset_controller.rb
+++ app/controllers/password_reset_controller.rb
@@ -9,7 +9,7 @@ class PasswordResetController < ApplicationController
     new_password = params[:new_password]
     new_password_confirmation = params[:new_password_confirmation]
 
-    if !user.present?
+    if !@user.present?
       redirect_to new_password_reset_path, alert: "Invalid token."
     elsif new_password != new_password_confirmation
       redirect_to new_password_reset_path, alert: "Two password inputs must match."
